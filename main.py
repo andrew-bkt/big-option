6063cf2aea8aa80b44a9571ea0ffa62b788dead2
andrew-bkt <andrew@brooklyn-techology.com>
2024-07-10 08:28:02 -0400

    CORS

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ from dotenv import load_dotenv
 from database import engine, get_db, Base
 from models import OptionData, OptionDataRequest, OptionDataResponse
 
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 load_dotenv()
 
 app = FastAPI()
